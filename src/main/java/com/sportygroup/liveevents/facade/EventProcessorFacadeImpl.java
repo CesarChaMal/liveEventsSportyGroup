7ab081e3b9c530e5d5f252ddc120c9fd36fabc36
Cesar Chavez <cesarchanalu@gmail.com>
2025-06-02 04:15:24 +0200

    Adding Circuit Breaker.

--- src/main/java/com/sportygroup/liveevents/facade/EventProcessorFacadeImpl.java
+++ src/main/java/com/sportygroup/liveevents/facade/EventProcessorFacadeImpl.java
@@ -3,6 +3,9 @@ package com.sportygroup.liveevents.facade;
 import com.sportygroup.liveevents.model.ScoreResponse;
 import com.sportygroup.liveevents.service.fetcher.EventFetcher;
 import com.sportygroup.liveevents.service.publisher.EventPublisher;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.util.Optional;
@@ -10,6 +13,7 @@ import java.util.Optional;
 @Component
 public class EventProcessorFacadeImpl implements EventProcessorFacade {
 
+    private static final Logger log = LoggerFactory.getLogger(EventProcessorFacadeImpl.class);
     private final EventFetcher eventFetcher;
     private final EventPublisher eventPublisher;
 
@@ -19,9 +23,15 @@ public class EventProcessorFacadeImpl implements EventProcessorFacade {
     }
 
     @Override
+    @CircuitBreaker(name = "scoreService", fallbackMethod = "fallbackProcessEvent")
     public void processEvent(String eventId) {
         Optional<ScoreResponse> score = eventFetcher.fetchScore(eventId);
         score.map(ScoreResponse::currentScore)
                 .ifPresent(currentScore -> eventPublisher.publish(eventId, currentScore));
+    }
+
+    public void fallbackProcessEvent(String eventId, Throwable t) {
+        log.warn("⚠️ Fallback triggered for event {} due to: {}", eventId, t.getMessage());
+        // Optional: Store fallback state or notify
     }
 }
